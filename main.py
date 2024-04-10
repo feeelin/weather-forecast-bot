a912d9a7cd87b29bc04d0e181f63cf83cec8cb45
Valery Filatov <valiklfiatoff@yandex.ru>
2024-04-10 17:04:31 +0300

    Logic has been deleted
    Удалил логику, чтобы потом можно было её прописать на мастер-классе
    
    Добавлен файлик с ключом к API, чтобы студенты не потерялись в документации к API
    
    Добавлены картинки для оформления бота в BotFather'е

--- main.py
+++ main.py
@@ -5,18 +5,3 @@ import logging
 from aiogram import Bot, Dispatcher
 from config import Settings
 from handlers import basic as basic_router
-
-
-dp = Dispatcher()
-
-
-async def main():
-    bot = Bot(token=Settings.BOT_TOKEN)
-    dp.include_router(basic_router.router)
-
-    await dp.start_polling(bot)
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
